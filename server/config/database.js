fc98cfea5d04ecf576ba4106e4a98563549b221d
gebriel-gd <gebrield@gmail.com>
2023-11-10 04:12:36 -0500

    database deployed

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,20 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-const pool = mysql.createPool({
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  connectionLimit: 10,
-});
 
-pool.getConnection(function (err, connection) {
-  console.log("Database connected!");
-});
+// const pool = mysql.createPool({
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   connectionLimit: 10,
+// });
+
+const pool = mysql.createConnection(process.env.DATABASE_URL);
+
+// pool.getConnection(function (err, connection) {
+//   console.log("Database connected!");
+// });
 
 let registration = `CREATE TABLE if not exists registration(
     user_id int auto_increment,
@@ -26,8 +29,7 @@ let profile = `CREATE TABLE if not exists profile(
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    PRIMARY KEY (user_profile_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    PRIMARY KEY (user_profile_id)
 )`;
 
 let question = `CREATE TABLE if not exists question(
@@ -37,8 +39,7 @@ let question = `CREATE TABLE if not exists question(
     question_code_block varchar(255),
     tags varchar(255),
     user_id int not null,
-    PRIMARY KEY (question_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    PRIMARY KEY (question_id)
 )`;
 
 let answer = `CREATE TABLE if not exists answer(
@@ -47,9 +48,7 @@ let answer = `CREATE TABLE if not exists answer(
     answer_code_block varchar(255),
     user_id int not null,
     question_id int not null,
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id),
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
+    PRIMARY KEY (answer_id)
 )`;
 
 pool.query(registration, (err, results) => {
